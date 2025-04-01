f795eb31404e3fa099c51c2ef3a9985e329a32b5
Marwan <marwan-walid1@hotmail.com>
2025-04-01 22:53:35 +0200

    Video 1

--- C43-G04-MVC03.DAL/Repositories/UnitOfWork.cs
+++ C43-G04-MVC03.DAL/Repositories/UnitOfWork.cs
@@ -4,21 +4,30 @@ public class UnitOfWork : IUnitOfWork
 {
     private readonly ApplicationDbContext _context;
     // Lazy loading doesn't depend on dependency injection
-    private Lazy<IEmployeeRepository> _employeeRepository;
-    private Lazy<IDepartmentRepository> _departmentRepository;
+    // private Lazy<IEmployeeRepository> _employeeRepository;
+    // private Lazy<IDepartmentRepository> _departmentRepository;
+    
+    private readonly Func<IEmployeeRepository> _employeerepositoryFactory;
+    private readonly Func<IDepartmentRepository> _departmentrepositoryFactory;
 
-    public UnitOfWork(ApplicationDbContext context)
+    public UnitOfWork(ApplicationDbContext context,
+        Func<IEmployeeRepository> employeerepositoryFactory,
+        Func<IDepartmentRepository> departmentrepositoryFactory)
     {
         _context = context;
-        _departmentRepository =
-            new Lazy<IDepartmentRepository>(() => new DepartmentRepository(context)); // Lazy loads dependencies
-        
-        _employeeRepository =
-            new Lazy<IEmployeeRepository>(() => new EmployeeRepository(context)); // Lazy loads dependencies
+        _employeerepositoryFactory = employeerepositoryFactory;
+        _departmentrepositoryFactory = departmentrepositoryFactory;
     }
 
-    public IEmployeeRepository Employees => _employeeRepository.Value; // Calls value factory upon retrieval 
-    public IDepartmentRepository Departments => _departmentRepository.Value;// Calls value factory upon retrieval 
+    // public IEmployeeRepository Employees => _employeeRepository.Value; // Calls value factory upon retrieval 
+    // public IDepartmentRepository Departments => _departmentRepository.Value;// Calls value factory upon retrieval 
+    
+    private IEmployeeRepository _employeeRepository;
+    private IDepartmentRepository _departmentRepository;
+    
+    // Like lazy loading except this decouples the classes or uses dependency injection
+    public IEmployeeRepository Employees => _employeeRepository ??= _employeerepositoryFactory.Invoke();
+    public IDepartmentRepository Departments => _departmentRepository ??= _departmentrepositoryFactory.Invoke();
     public int SaveChanges() => _context.SaveChanges();
 
     // Gets called by Default from the clr with the end of the lifetime of the object
--- C43-G04-MVC03.Presentation/Program.cs
+++ C43-G04-MVC03.Presentation/Program.cs
@@ -1,4 +1,3 @@
-
 using C43_G04_MVC03.Presentation.Services;
 
 namespace C43_G04_MVC03.Presentation;
@@ -16,19 +15,25 @@ public class Program
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             options.UseSqlServer(connectionString);
         });
-        
-        // builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-        // builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+        builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+        builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
         builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
         builder.Services.AddScoped<IDepartmentService, DepartmentService>();
         builder.Services.AddScoped<IEmployeeService, EmployeeService>();
         builder.Services.AddAutoMapper(typeof(AssemblyReference).Assembly);
         
-        
+        builder.Services.AddScoped<Func<IDepartmentRepository>>(provider =>
+            () => provider.GetRequiredService<IDepartmentRepository>());
+
+        builder.Services.AddScoped<Func<IEmployeeRepository>>(provider =>
+            provider.GetRequiredService<IEmployeeRepository>);
+
+
         // LifeTime services
-        builder.Services.AddSingleton<ISingletonSerivce, SingletonService>(); // Creates one instance per application
-        builder.Services.AddScoped<IScopedService, ScopedService>(); // Creates one instance per request
-        builder.Services.AddTransient<ITransientService, TransientService>(); // Creates one instance per operation
+        // builder.Services.AddSingleton<ISingletonSerivce, SingletonService>(); // Creates one instance per application
+        // builder.Services.AddScoped<IScopedService, ScopedService>(); // Creates one instance per request
+        // builder.Services.AddTransient<ITransientService, TransientService>(); // Creates one instance per operation
 
         var app = builder.Build();
 
